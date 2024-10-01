4aa6ebb9dbaa3dc9097ac4c9905c80c30567df6a
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2024-10-01 14:10:29 +0200

    1 y 2 resueltos

--- ej05.repaso1-4/script.js
+++ ej05.repaso1-4/script.js
@@ -6,3 +6,39 @@ Crea un documento HTML que solicite cuántos caramelos tienes
  tienes que repartir __ caramelos a cada uno
  y te sobran __ caramelos".
 */
+
+const numCaramelosInput = document.getElementById("numCaramelosInput")
+const numPersonasInput = document.getElementById("numPersonasInput")
+const resultadoTA = document.getElementById("resultadoTA")
+
+const botones = document.getElementsByTagName("button")
+const miBoton = botones[0]
+
+numCaramelosInput.addEventListener("keyup",function(ev){
+    if (ev.key == "Enter") {
+        numPersonasInput.focus()
+    }
+})
+numPersonasInput.addEventListener("keyup",function(ev){
+    if (ev.key == "Enter") {
+        comprobarYcalcular()
+    }
+})
+miBoton.addEventListener("click",comprobarYcalcular)
+
+function comprobarYcalcular() {
+    //comprobar que hay 2 numeros correctos escritos y dividir
+    let numCar = parseInt(numCaramelosInput.value)
+    let numPer = parseInt(numPersonasInput.value)
+    if (numCar >= 0 && numPer > 0) {
+        let parteEntera = Math.floor(numCar / numPer)
+        let restantes = numCar % numPer
+        // resultadoTA.value = "Repartes " + parteEntera
+        //     + " caramelos a cada persona y te sobran "
+        //     + restantes + " caramelos"
+        
+        resultadoTA.value = `Repartes ${parteEntera} caramelos a cada persona y te sobran ${restantes} caramelos`
+    } else {
+        resultadoTA.value = "Debes escribir números enteros positivos en los 2 campos"
+    }
+}
--- ej05.repaso1-4/script2.js
+++ ej05.repaso1-4/script2.js
@@ -4,3 +4,74 @@
  y escriba en el documento cuál es
  la diferencia entre el mayor y el menor.
 */
+
+const numeroInput = document.getElementById("numeroInput")
+const numerosTA = document.getElementById("numerosTA")
+const resultadoTA = document.getElementById("resultadoTA")
+const calcularBtn = document.getElementById("calcularBtn")
+const vaciarBtn = document.getElementById("vaciarBtn")
+
+let misNumeros = []
+
+numeroInput.focus()
+numeroInput.addEventListener("keyup", function(ev) {
+    if (ev.key == "Enter") {
+        if (!isNaN(numeroInput.value)) {
+            //si es un nº lo meto al array
+            misNumeros.push(numeroInput.value)
+            numerosTA.value = misNumeros
+            //vaciar la caja de texto de entrada
+            numeroInput.value = ""
+        }
+    }
+})
+
+calcularBtn.addEventListener("click",function(){
+    const max = Math.max(...misNumeros)
+    // Math.max(misNumeros[0],misNumeros[1], ..... misNumeros[n-1])
+    const min = Math.min(...misNumeros)
+    resultadoTA.value = `La diferencia entre ${max} y ${min} es ${max-min}.`
+})
+
+vaciarBtn.addEventListener("click",function(){
+    numerosTA.value = ""
+    misNumeros = []
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// INPUT / TEXTAREA / SELECT / CHECKBOXES / RADIOBUTTON
+
+//     .value
+
+
+// H1 / SPAN / DIV / LABEL / P / BUTTON
+
+//     .textContent
+//     .innerHTML
+
+//     miLabel.textContent = "Hola"
+//     miLabel.textContent = "<b>hola</b>"
+
+//     en el HTML, dentro del label aparecerá
+//     "Hola" o "<b>hola</b>"
+
+//     miLabel.innerHTML = "<b>hola</b>"
+//         "hola" en negrita
+
+
+
+
+
+
+
+
