98bfc1a2ac3221ae861a83144b403869e2fade00
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2024-10-02 12:27:31 +0200

    reto 4 practicamente hecho

--- ej05.repaso1-4/script4.js
+++ ej05.repaso1-4/script4.js
@@ -6,7 +6,7 @@
   generará más números aleatorios entre 1 y 11. Si el usuario
   acumula más de 21 puntos directamente ha perdido.
   Si el jugador deja de pedir números antes de sobrepasar el 21,
-  entonces el programa generará un número aleatorio entre 14 y 21
+  entonces el programa generará cartas aleatorias para el crupier
   para competir contra el jugador y decidir quién ha ganado
   la partida.
 */
@@ -14,22 +14,72 @@
 const cartaBtn = document.getElementById("cartaBtn")
 const plantarseBtn = document.getElementById("plantarseBtn")
 const manoUl = document.getElementById("manoUl")
+const marcadorDiv = document.getElementById("marcadorDiv")
+const resultadoTA = document.getElementById("resultadoTA")
+
 let partidaEnMarcha = false
 let puntos
 
 cartaBtn.addEventListener("click",function(){
   if (partidaEnMarcha == false) {
     puntos = 0
+    resultadoTA.textContent = ""
     partidaEnMarcha = true
-    //this = cartaBtn dentro del ambito del function del addeventlistener
+    plantarseBtn.disabled = false
+    //vaciar las cartas de la partida anterior
+    manoUl.innerHTML = ""
+    marcadorDiv.textContent = puntos + " puntos"
     this.textContent = "Pedir otra carta"
+    //this = cartaBtn dentro del ambito del function del addeventlistener
     //al principio se dan 2 cartas
     darCarta()
     darCarta()
+    //Caso particular: sacas un 21 de primeras --> BLACKJACK
+    //(sin hacer)
   } else {
     darCarta()
   }
 })
+
+plantarseBtn.addEventListener("click",function(){
+  partidaEnMarcha = false
+  let puntosCrupier = 0
+  while (puntosCrupier < 17 || puntosCrupier > 21) {
+    const cartaRandom = Math.floor(Math.random()*11) + 1
+    puntosCrupier += cartaRandom
+  }
+  if (puntosCrupier > 21 ){
+    //mostrar que el jugador ha ganado
+    resultadoTA.textContent = "La casa ha obtenido " + puntosCrupier + " puntos. Ganas la partida."
+  } else {
+    //comparar puntuaciones para decidir quién ha ganado
+    if (puntos > puntosCrupier)
+      resultadoTA.textContent = "Ganas. La casa tiene " + puntosCrupier + " puntos."
+    else if (puntos == puntosCrupier) {
+      resultadoTA.textContent = "Empatas. La casa tiene " + puntosCrupier + " puntos."
+    } else {
+      resultadoTA.textContent = "Pierdes. La casa tiene " + puntosCrupier + " puntos."
+    }
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function darCarta() {
   const cartaRandom = Math.floor(Math.random()*11) + 1
@@ -38,4 +88,15 @@ function darCarta() {
   //Por otro lado, mostrar la carta en el HTML
   const nuevoLi = document.createElement("li")
   nuevoLi.textContent = cartaRandom
+  nuevoLi.classList.add("list-group-item")
+  manoUl.append(nuevoLi)
+  marcadorDiv.textContent = puntos + " puntos"
+  //comprobar si nos hemos pasado de 21
+  if (puntos > 21) {
+    partidaEnMarcha = false
+    cartaBtn.textContent = "Empezar"
+    resultadoTA.textContent = "Te has pasado de 21. Pierdes la partida."
+    //deshabilitar boton Plantarse
+    plantarseBtn.disabled = true
+  }
 }
