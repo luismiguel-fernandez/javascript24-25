0b66dfda813f7b3232b323cbd5997567db6ae247
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2024-10-18 14:28:52 +0200

    ej09 progresando

--- DWECD/ej03/script.js
+++ DWECD/ej03/script.js
@@ -2,15 +2,54 @@ const formu = document.getElementsByTagName("form")[0]
 
 formu.addEventListener("submit",function(ev){
     ev.preventDefault()
+
     let todoOk = true
+
+
+    const expRegNombre = /^[a-zA-Z][\w]*(\.[a-z]{3})?$/
     //comprobar los campos que consideremos oportunos
     const nombre = document.getElementById("nombreInput")
-    if (nombre.value.trim().length <= 2) {
+    if ( !expRegNombre.test( nombre.value.trim() ) ) {
         todoOk = false
         //avisar al usuario del error en el campo nombre
     } else {
         //borrar el mensaje de error del campo nombre
     }
+
+
+
+
+
+    const expRegMatricula = /^\d{4}[ -]?[]{3}$/
+    //comprobar los campos que consideremos oportunos
+    const matricula = document.getElementById("matriculaInput")
+    if ( !expRegMatricula.test( matricula.value.trim() ) ) {
+        todoOk = false
+        //avisar al usuario del error en el campo nombre
+    } else {
+        //borrar el mensaje de error del campo nombre
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     const generoSelect = document.getElementById("generoSelect")
     if (generoSelect.value == "0") {
         todoOk = false
--- ej09.phonebook/script.js
+++ ej09.phonebook/script.js
@@ -1,1 +1,66 @@
-alert("Primera instrucción javascript")
+//recuperar elementos del arbol DOM
+const inputName = document.querySelector("#inputName")
+const inputNumber = document.querySelector("#inputNumber")
+const btnAdd = document.querySelector("#btnAdd")
+const phonebookTbody = document.querySelector("#phonebooktable>tbody")
+const phonebook = []
+
+//código automático (sin eventos de usuario); se ejecuta al cargar la página
+resetForm()
+
+
+//dejar programados los eventos de usuario
+inputName.addEventListener("keyup",function(ev){
+    if (ev.key == "Enter") {
+        inputNumber.focus()
+    }
+})
+
+btnAdd.addEventListener("click",function(){
+    let name = inputName.value.trim()
+    let number = inputNumber.value.trim()
+    addContactToPhonebook(name, number)
+    refreshPhonebookTable(name, number)
+    resetForm()
+})
+
+inputNumber.addEventListener("keyup",function(ev){
+    if (ev.key == "Enter") {
+        let name = inputName.value.trim()
+        let number = inputNumber.value.trim()
+        addContactToPhonebook(name,number)
+        refreshPhonebookTable(name,number)
+        resetForm()
+    }
+})
+
+
+//funciones auxiliares
+function addContactToPhonebook(name,number) {
+    const expRegNumber = /^(([0-9]{4})|(\+[0-9]{2,3}))?[0-9]{9}$/
+        // 123456789 || 0034123456789 || +34123456789
+    if ( name && expRegNumber.test(number) ){
+        phonebook.push(
+            {
+                name: name,
+                number: number
+            }
+        )
+    } 
+}
+
+function refreshPhonebookTable(name,number) {
+    let newTR = phonebookTbody.insertRow()
+    let newTD1 = newTR.insertCell()
+    let newTD2 = newTR.insertCell()
+    let newTD3 = newTR.insertCell()
+    newTD1.textContent = name
+    newTD2.textContent = number
+}
+
+function resetForm() {
+    inputName.value = ""
+    inputNumber.value = ""
+    inputName.focus()
+}
+
