25c27df92c55cbb9e94c262a044efcbc7a21a691
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2024-11-06 10:13:31 +0100

    ej14 planteado

---
+++ ej13.atrapaLaBola/script.js
@@ -0,0 +1,13 @@
+const ANCHURA_TABLERO = 600
+const ALTURA_TABLERO = 300
+const DIAMETRO_BOLA = 30
+
+/*
+records = [
+    {name: "Andrew", points: 10},
+    {name: "Pamela", points: 8},
+    {name: "Elisabeth", points: 6},
+    {name: "George", points: 4},
+    {name: "Caroline", points: 2}
+]
+*/
