c6fa3ebec977a1628abdd1d3352e76f21a98cc8c
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2024-11-06 12:28:40 +0100

    ej13 a falta de records

--- ej13.atrapaLaBola/script.js
+++ ej13.atrapaLaBola/script.js
@@ -1,13 +1,16 @@
 const ANCHURA_TABLERO = 600
 const ALTURA_TABLERO = 300
 const DIAMETRO_BOLA = 30
+const DURACION = 10
 
 const btnEmpezar = document.querySelector("#btnEmpezar")
 const tablero = document.querySelector("#tablero")
 const bola = document.querySelector("#bola")
 const labelPuntos = document.querySelector("#puntos")
+const labelVelocidad = document.querySelector("#velocidad")
+const labelTiempo = document.querySelector("#tiempo")
 
-let tiempo = 10
+let tiempo
 let partida = false
 let puntos = 0
 labelPuntos.textContent = puntos
@@ -18,24 +21,44 @@ bola.style.width = DIAMETRO_BOLA + "px"
 bola.style.height = DIAMETRO_BOLA + "px"
 
 let movimiento
+let crono
 
 btnEmpezar.addEventListener("click" , function(){
     partida = true
+    clearInterval(movimiento)
     movimiento = setInterval(moverBola,1000)
     puntos = 0
     labelPuntos.textContent = puntos
+    labelVelocidad.textContent = 1000
+    tiempo = DURACION
+    labelTiempo.textContent = tiempo
+    clearInterval(crono)
+    crono = setInterval(function(){
+        tiempo--
+        labelTiempo.textContent = tiempo
+        if(tiempo == 0){
+            clearInterval(crono)
+            clearInterval(movimiento)
+            partida = false
+        }
+    },1000)
 })
 
 tablero.addEventListener("click", function(ev){
     if(ev.target.id != "bola"){
         clearInterval(movimiento)
         partida = false
+        clearInterval(crono)
     }
 })
 
 bola.addEventListener("click", function(){
     if(partida){
         puntos++
+        moverBola()
+        clearInterval(movimiento)
+        movimiento = setInterval(moverBola,1000 - puntos*50)
+        labelVelocidad.textContent = 1000 - puntos*50
         labelPuntos.textContent = puntos
     }
 })
