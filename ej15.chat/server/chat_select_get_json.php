f3df431cbdcae14a26bb959773a9336d258bfc0c
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2024-11-19 12:58:47 +0100

    php getter actualizado

--- ej15.chat/server/chat_select_get_json.php
+++ ej15.chat/server/chat_select_get_json.php
@@ -1,72 +1,35 @@
 <?php
 if(isset($_GET['ultimo'])) {
 
-    class cuaterna {
-        var $ids;
-        var $nicks;
-        var $textos;
-        var $fechas;
+    class mensaje {
+        var $id;
+        var $nick;
+        var $texto;
+        var $fecha;
     }
-    function queryIDs($pattern,$conexion) {
-        $sentencia = "SELECT id FROM mensajes WHERE id > ".$_GET['ultimo'];
-        $resultados = mysql_query($sentencia, $conexion) or die("Error en query: ".mysql_error());
-        $salida = array();
-        while ($fila = mysql_fetch_array($resultados)) {
-           $salida[] = $fila['id'];
+    function queryMensajes($ultimo,$conexion) {
+        $consulta = "SELECT * FROM mensajes WHERE id > ".$ultimo ;
+        $sen = $conexion->prepare($consulta);
+        $sen->execute();
+        $result = [];
+        while($row = $sen->fetch(PDO::FETCH_NAMED)){
+            $rec = new mensaje();
+            $rec->id = $row['id'];
+            $rec->fecha = $row['instante'];
+            $rec->nick = $row['nick'];
+            $rec->texto = $row['mensaje'];
+            $result[] = $rec;
         }
-        return $salida;
-    }
-    function queryNicks($pattern,$conexion) {
-        $sentencia = "SELECT nick FROM mensajes WHERE id > ".$_GET['ultimo'];
-        $resultados = mysql_query($sentencia, $conexion) or die("Error en query: ".mysql_error());
-        $salida = array();
-        while ($fila = mysql_fetch_array($resultados)) {
-           $salida[] = utf8_encode($fila['nick']);
-        }
-        return $salida;
-    }
-    function queryTextos($pattern,$conexion) {
-        $sentencia = "SELECT texto FROM mensajes WHERE id > ".$_GET['ultimo'];
-        $resultados = mysql_query($sentencia, $conexion) or die("Error en query: ".mysql_error());
-        $salida = array();
-        while ($fila = mysql_fetch_array($resultados)) {
-           $salida[] = $fila['texto'];
-        }
-        return $salida;
-    }
-    function queryFechas($pattern,$conexion) {
-        $sentencia = "SELECT instante FROM mensajes WHERE id > ".$_GET['ultimo'];
-        $resultados = mysql_query($sentencia, $conexion) or die("Error en query: ".mysql_error());
-        $salida = array();
-        while ($fila = mysql_fetch_array($resultados)) {
-           $salida[] = $fila['instante'];
-        }
-        return $salida;
-    }
-    function queryAll($pattern,$conexion) {
-        $cuaternaResult = new cuaterna();
-        $cuaternaResult->ids = queryIDs($pattern,$conexion);
-        $cuaternaResult->nicks = queryNicks($pattern,$conexion);
-        $cuaternaResult->textos = queryTextos($pattern,$conexion);
-        $cuaternaResult->fechas = queryFechas($pattern,$conexion);
-        return $cuaternaResult;
+        return $result;
     }
 
-    $servidor = 'localhost';
-	$bd = 'chat';
-	$user = 'root';
-	$pw = '';
-	$con = mysql_connect($servidor,$user,$pw);
-	mysql_select_db($bd, $con);
-	mysql_set_charset('utf8');
-
-    $server = "mysql:dbname=atrapaLaBola";
+    $server = "mysql:dbname=chat";
 	$user = "root";
 	$pw = "";
 	$con = new PDO($server,$user,$pw,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
 
-    
-    echo json_encode( queryAll($_GET['ultimo'],$con) );
+   
+    echo json_encode( queryMensajes($_GET['ultimo'],$con) );
 } else {
 	echo "No has pasado los parámetros correctos. Debes pasar 'ultimo'";
 }
